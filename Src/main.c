fafa03c40e239956074c0bfe00aaf7c2614ee0b4
ColtonTshudy <coltont@vt.edu>
2024-09-11 23:58:15 -0400

    changed _write imeplementation

--- Src/main.c
+++ Src/main.c
@@ -243,9 +243,28 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+/**
+ * @brief Overrider to pipe printf to USB-CTC
+ *
+ * @param file
+ * @param ptr
+ * @param len
+ * @return int
+ */
 int _write(int file, char *ptr, int len)
 {
-  CDC_Transmit_FS((uint8_t *)ptr, len);
+  static uint8_t rc = USBD_OK;
+  do
+  {
+    rc = CDC_Transmit_FS(ptr, len);
+  } while (USBD_BUSY == rc);
+
+  if (USBD_FAIL == rc)
+  {
+    /// NOTE: Should never reach here.
+    /// TODO: Handle this error.
+    return 0;
+  }
   return len;
 }
 /* USER CODE END 4 */
